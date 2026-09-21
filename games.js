4cfecc284dc05fe9673a7936d0b6a0dfbaaad271
minicartuchosretro <miniretrocartuchos@gmail.com>
2026-09-21 15:50:31 -0300

    Add files via upload

---
+++ games.js
@@ -0,0 +1,45 @@
+const GAMES = {
+  "doom": { 
+    title: "DOOM", 
+    rom: "./roms/doom.sfc", 
+    system: "snes", 
+    icon: "./icons/doom.png",
+    hidden: true,
+    desc: "El clásico shooter que definió el género. Enfréntate a las hordas demoníacas en las instalaciones de Marte en esta brutal e icónica aventura secreta."
+  },
+  "mk2":  { 
+    title: "Mortal Kombat II", 
+    rom: "./roms/mk2.sfc", 
+    system: "snes", 
+    icon: "./icons/mk2.png",
+    desc: "El sangriento torneo continúa. Descubre nuevos luchadores, escenarios y Fatalities en uno de los juegos de lucha más legendarios de la historia."
+  },
+  "dkc1": { 
+    title: "Donkey Kong Country", 
+    rom: "./roms/dkc1.smc", 
+    system: "snes", 
+    icon: "./icons/dkc1.png",
+    desc: "Acompaña a Donkey Kong y Diddy Kong en una aventura épica y visualmente revolucionaria para recuperar su reserva de bananas robada por los Kremlings."
+  },
+  "dkc2": { 
+    title: "Donkey Kong Country 2", 
+    rom: "./roms/dkc2.sfc", 
+    system: "snes", 
+    icon: "./icons/dkc2.png",
+    desc: "¡Diddy y Dixie al rescate! Explora los peligros de la Isla Cocodrilo y enfréntate al Capitán K. Rool para salvar al mismísimo Donkey Kong."
+  },
+  "dkc3": { 
+    title: "Donkey Kong Country 3", 
+    rom: "./roms/dkc3.sfc", 
+    system: "snes", 
+    icon: "./icons/dkc3.png",
+    desc: "Dixie y el pequeño Kiddy Kong se adentran en el misterioso Kremisferio Norte. Descubre nuevos secretos y vehículos en el gran cierre de la trilogía."
+  },
+  "ki":   { 
+    title: "Killer Instinct", 
+    rom: "./roms/ki.sfc", 
+    system: "snes", 
+    icon: "./icons/ki.png",
+    desc: "Lucha sin cuartel con gráficos renderizados de vanguardia. Domina a personajes icónicos y ejecuta combos interminables. ¡C-C-C-Combo Breaker!"
+  }
+};
---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'retroc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './games.js',
+  './logo.png',
+  './icon.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    }).catch(() => {
+      // Si falla la red y no está en caché, no hace nada fatal
+    })
+  );
+});
